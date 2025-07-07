9e9823ce47c7612b581146866fa605de94df1ab5
Nikita Vlasov <nikistosov@gmail.com>
2025-07-07 10:45:54 +0200

    feat: add libcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { resolve } from 'path';
 
 import { defineConfig } from 'vite';
 
+import libCss from 'vite-plugin-libcss';
+
 import { dependencies, devDependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [libCss()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
